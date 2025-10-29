86cda247d96b06fecb9662cd266cfe974bdbc8e4
paulo <paulo.nasr00@gmail.com>
2025-10-29 13:08:33 +0100

    updatering på lab2

--- lab2/cirkel.py
+++ lab2/cirkel.py
@@ -25,7 +25,7 @@ class Circle:
 
         # equal/lika med
         def __eq__(self, other):
-             return self.radius == other.radius #kollar bara radien för att kolla om cirklarna är lika stora
+             return self.radius == other.radius #kollar om radien är lika med den andra cirkeln
         
         # greater than/större än
         def __gt__(self, other):
@@ -36,4 +36,14 @@ class Circle:
         #större än eller lika med
         def __ge__(self, other):
             return self.radius >= other.radius
+        
+        def __repr__(self):# skriva ut för kodaren
+            return f"Circle({self.x}, {self.y}, {self.radius})"
+        
+        def __str__(self):# skriv ut cirkelns position och radie för användaren
+            return f"Cirkelns på position ({self.x}, {self.y}) med radie {self.radius}"
+         
+        def is_unit_circle(self): #kollar om det är enhetscirkel
+            return self.radius == 1 and self.x == 0 and self.y == 0
+
         
